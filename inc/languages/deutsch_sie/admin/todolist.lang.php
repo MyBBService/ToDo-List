32ecba29d236cdc7b83d78907d4af03f9b3aee76
Kenny <admin@mybbservice.de>
2012-11-11 12:54:20 +0100

    deutsch_sie hinzugefügt

---
+++ inc/languages/deutsch_sie/admin/todolist.lang.php
@@ -0,0 +1,33 @@
+<?php
+//Plugins
+$l['name_plugins'] = "Aufgaben-Liste";
+$l['description_plugins'] = "Es bietet eine Liste f&uuml;r ihre zu erledigenden Aufgaben.";
+
+//Setting-Overview
+$l['name_settingoverview'] = "Aufgaben-Liste";
+$l['description_settingoverview'] = "Einstellungen f&uuml;r die Aufgaben-Liste";
+
+//Setting #1
+$l['name_setting1'] = "Aktivierung";
+$l['desc_setting1'] = "Aufgabenliste aktivieren?";
+
+//Setting #2
+$l['name_setting2'] = "Erlaube G&auml;sten den Zugriff";
+$l['desc_setting2'] = "M&ouml;chten Sie G&auml;sten den Zugriff erlauben?";
+
+//Setting #3
+$l['name_setting3'] = "Verweigere bestimmten Gruppen den Zugriff";
+$l['desc_setting3'] = "Geben Sie bitte Gruppen-IDs ein, um diesen Gruppen den Zugriff zu verweigern. Mit Kommata trennen.";
+
+//Setting #4
+$l['name_setting4'] = "Moderation";
+$l['desc_setting4'] = "Geben Sie bitte die Gruppen-IDs ein, um diesen Gruppen die Moderationsrechte zu geben. Mit Kommata trennen.";
+
+//Setting #5
+$l['name_setting5'] = "Kann Aufgaben hinzuf&uuml;gen";
+$l['desc_setting5'] = "Geben Sie die Gruppen-IDs ein, die Aufgaben hinzuf&uuml;gen koennen. Mit Kommata trennen.";
+
+//Setting #6
+$l['name_setting6'] = "Angezeigter Name";
+$l['desc_setting6'] = "Geben Sie hier den Namen ein, der angezeigt werden soll. Beispiel: Interne Aufgaben-Liste";
+?>
