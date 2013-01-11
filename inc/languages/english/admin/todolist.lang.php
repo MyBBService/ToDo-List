0069d13097f4de86c755c058a2160e0e997a8b96
JN-Jones <jones_h1@web.de>
2013-01-11 21:47:05 +0100

    Deutsche Sprachdateien überarbeitet & Englische eingefügt

--- inc/languages/english/admin/todolist.lang.php
+++ inc/languages/english/admin/todolist.lang.php
@@ -1,33 +1,92 @@
 <?php
-//Plugins
-$l['name_plugins'] = "Todolist";
-$l['description_plugins'] = "It offer a list for your todos";
+//Plugin
+$l['todo'] = "ToDo-List";
+$l['todo_permissions'] = "Can manage ToDo-List";
+$l['todo_no_id'] = "For this action is an ID required which has not been transmitted.";
+$l['todo_wrong_id'] = "The transmitted ID does not appear to be unique";
 
-//Setting-Overview
-$l['name_settingoverview'] = "Todolist";
-$l['description_settingoverview'] = "Settings for your todolist";
+//Menu
+$l['todo_list'] = "Projectlisting";
+$l['todo_list_desc'] = "Displays a list of all projects";
+$l['todo_add'] = "Add a new project";
+$l['todo_add_desc'] = "Here you can add a new project";
+$l['todo_search'] = "Searchqueries";
+$l['todo_search_desc'] = "Displays a list of all searchqueries";
+$l['todo_search_add'] = "Add searchquery";
+$l['todo_search_add_desc'] = "Herewith you can create predefined searchqueries";
+$l['todo_edit'] = "Edit Project";
+$l['edit_success'] = "Project successfully edited";
+$l['todo_deleted'] = "Project deleted";
+$l['todo_delete_confirm'] = "Are you sure you want to delete this project?<br />
+<span style=\"text-color: #FFFFF; font-weight: bold;\">ATTENTION:</span> This will also delete all ToDos in this project";
 
-//Setting #1
-$l['name_setting1'] = "Activation";
-$l['desc_setting1'] = "Activate todolist?";
+//Listing
+$l['todo_item'] = "Project";
+$l['todo_todos'] = "Number of ToDos in this project";
+$l['no_projects'] = "There are no projects available";
 
-//Setting #2
-$l['name_setting2'] = "Access for guests";
-$l['desc_setting2'] = "Do you allow the access for guests?";
+//Add
+$l['todo_title'] = "Title";
+$l['todo_title_desc'] = "How to call the project?";
+$l['missing_title'] = "Please enter a title for the project";
+$l['todo_desc'] = "Description";
+$l['todo_desc_desc'] = "Describe briefly the project";
+$l['missing_desc'] = "Please describe the project";
+$l['todo_permissions'] = "Permissions";
+$l['group'] = "Usergroup";
+$l['can_see'] = "Can view project";
+$l['can_add'] = "Can add new ToDos";
+$l['can_edit'] = "Can edit ToDos";
+$l['todo_save'] = "Save prokect";
+$l['add_success'] = "Project successfully saved";
 
-//Setting #3
-$l['name_setting3'] = "Disallow for specific groups the access";
-$l['desc_setting3'] = "Enter the Group-IDs, to refuse the access. Separate with commas.";
+//Settings
+$l['setting_group_todo'] = "ToDo-List";
+$l['setting_group_todo_desc'] = "Settings for ToDo-List";
+$l['setting_todo_activate'] = "Activate?";
+$l['setting_todo_activate_desc'] = "";
+$l['setting_todo_name'] = "Displayed name";
+$l['setting_todo_name_desc'] = "Enter here the name, who will display. Example: My Todolist";
+$l['setting_todo_per_page'] = "Entries per page";
+$l['setting_todo_per_page_desc'] = "Enter here the number of entries to be displayed per page";
+$l['setting_todo_404_errors'] = "Should we display 404 Errors?";
+$l['setting_todo_404_errors_desc'] = "If this setting is enabled, instead of the \"no_permission\" errorpage 404(page not exist) errors will be displayed.<br />This can help if the ToDo list is not meant for the public";
+$l['setting_todo_pm_notify'] = "Send a PM when the Assignment is changed?";
+$l['setting_todo_pm_notify_desc'] = "Should the users be notified when a ToDo is assigned to them?";
 
-//Setting #4
-$l['name_setting4'] = "Moderation";
-$l['desc_setting4'] = "Enter the Group-IDs, to give them the moderation-rights. Separate with commas.";
+//Search
+$l['todo_url'] = "URL";
+$l['no_searchs'] = "There are no Searchqueries available";
 
-//Setting #5
-$l['name_setting5'] = "Can add todos";
-$l['desc_setting5'] = "Enter the Group-IDs, to give them the rights to add todos. Separate with commas.";
+//Search Add
+$l['todo_search_title_desc'] = "How should the Searchquery be named?";
+$l['todo_string'] = "Searchstring";
+$l['todo_string_desc'] = "Which string have to appear in the ToDo (Title or Description)?";
+$l['todo_status'] = "Status";
+$l['todo_status_desc'] = "Which status should the ToDp have?";
+$l['todo_creator'] = "Creator";
+$l['todo_creator_desc'] = "Who should have created the ToDo?";
+$l['todo_assign'] = "Assigned";
+$l['todo_assign_desc'] = "Who needs to be assigned to the ToDo?";
+$l['todo_project'] = "Project";
+$l['todo_project_desc'] = "In which project should the ToDo be?";
+$l['todo_priority'] = "Priority";
+$l['todo_priority_desc'] = "Which priority should the ToDo have?";
+$l['todo_save_search'] = "Save Searchquery";
+$l['missing_search_title'] = "Please enter a title for the searchquery";
+$l['search_add_success'] = "Searchquery successfully saved";
+$l['todo_search_deleted'] = "Searchquery deleted";
+$l['todo_search_delete_confirm'] = "Are you sure you want to delete the searchquery?";
+$l['search_edit_success'] = "Searchquery successfully edited";
+$l['todo_search_edit'] = "Edit Searchquery";
 
-//Setting #6
-$l['name_setting6'] = "Displayed name";
-$l['desc_setting6'] = "Enter here the name, who will display. Example: My Todolist";
+$l['priority_normal'] = "Normal";
+$l['priority_high'] = "High";
+$l['priority_low'] = "Low";
+
+$l['status_wait'] = "Waiting";
+$l['status_dev'] = "In Development";
+$l['status_feed'] = "Feedback";
+$l['status_resolved'] = "Resolved";
+$l['status_closed'] = "Closed";
 ?>
--- inc/languages/english/todolist.lang.php
+++ inc/languages/english/todolist.lang.php
@@ -1,31 +1,32 @@
 <?php
-$l['offline'] = "Todolist is disabled. Please try it again later.";
+$l['offline'] = "ToDo-List is disabled. Please try it again later.";
 
 //Overview
-$l['title_overview'] = "Todolist";
+$l['title_overview'] = "ToDo-List";
 $l['add_todo'] = "Add new todo";
 $l['delete_todo'] = "Delete";
 $l['edit_todo'] = "Edit";
 $l['show_todo'] = "Show";
-$l['moderation_todo'] = "Will be moderated by";
 $l['date_todo'] = "Written at";
 $l['title_todo'] = "Title";
 $l['done_todo'] = "% done";
+$l['assign_todo'] = "Assigned";
 $l['status_todo'] = "Status";
 $l['from_todo'] = "Written by";
 $l['priority_todo'] = "Priority";
 $l['action_todo'] = "Action";
 $l['description_todo'] = "Description";
-$l['no_todo'] = "Unfortunately, no todos are registered.";
-$l['added_todo'] = "Todo added!";
-$l['deleted_todo'] = "Todo deleted!";
+$l['no_todo'] = "Unfortunately no todos are registered.";
+$l['no_projects'] = "Unfortunately no projects were added.";
+$l['assign_none'] = "Nobody";
+$l['added_todo'] = "ToDo added!";
+$l['edited_todo'] = "ToDo edited!";
+$l['deleted_todo'] = "ToDo deleted!";
 
 //Show Todo
 $l['show_showtodo'] = "Show todo";
 $l['lastedit_showtodo'] = "Last edit";
 $l['back_showtodo'] = "Back to overview";
-$l['today_showtodo'] = "Today";
-$l['yesterday_showtodo'] = "Yesterday";
 $l['from_showtodo'] = "from";
 
 //Edit Todo
@@ -53,4 +54,41 @@ $l['done_25'] = "25% done";
 $l['done_50'] = "50% done";
 $l['done_75'] = "75% done";
 $l['done_100'] = "100% done";
+
+//Errors
+$l['no_title'] = "Please enter a title!";
+$l['no_done'] = "Please select how much has been already done!";
+$l['no_status'] = "Please selct a status!";
+$l['no_priority'] = "Please select a priority!";
+$l['no_message'] = "Please enter a description!";
+
+//Assign PN
+$l['notify_subject'] = "You were assigned to a todo";
+$l['notify_message'] = "Hello {1},
+you were assigned to the todo [url={4}]{5}[/url] by [url={2}]{3}[/url].";
+
+//WIO Liste
+$l['todo_wol'] = "Views the <a href=\"todolist.php\">ToDo-List</a>";
+$l['todo_wol_add'] = "Adds a new ToDo";
+$l['todo_wol_show'] = "Views the ToDo <a href=\"todolist.php?action=show&id={2}\">{1}</a>";
+$l['todo_wol_show_project'] = "Views the project <a href=\"todolist.php?action=show_project&id={2}\">{1}</a>";
+$l['todo_wol_delete'] = "Deletes a ToDo";
+$l['todo_wol_edit'] = "Edits the ToDo <a href=\"todolist.php?action=show&id={2}\">{1}</a>";
+$l['todo_wol_search'] = "<a href=\"todolist.php?action=search\">Searchs</a> in our ToDo-List";
+
+//Suche
+$l['search'] = "Search";
+$l['search_string'] = "Searchstring";
+$l['search_status'] = "Status";
+$l['search_creator'] = "Creator";
+$l['search_assign'] = "Assigned";
+$l['search_project'] = "Project";
+$l['search_priority'] = "Priority";
+$l['search_do'] = "Start Search!";
+$l['search_results'] = "Searchresults";
+$l['search_results_nothing'] = "Unfortunately nothing was found with your settings";
+$l['searches'] = "Searchqueries";
+
+$l['confirm_delete'] = "Confirm deleting";
+$l['confirm_delete_desc'] = "Are you sure you want to delete this ToDo?";
 ?>
